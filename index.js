c6b7da29257edb00ff9f535d3b812d0029eb3271
Kuba Karmiński <jlkarminski@gmail.com>
2020-11-13 16:08:15 +0100

    Perform the input conversion

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 class RomanNumber {
   constructor(input) {
     this.validate(input);
+    if (typeof input === "string") {
+      this.validateRomanNumber(input);
+      this.data = this.convertToDecimal(input);
+    } else {
+      this.data = input;
+    }
   }
 
   validate(input) {
+    // check empty values
     if (input === null || input === "") {
       throw Error("value required");
     }
 
+    // check input is a valid int or a string
     if (
       typeof input === "number" &&
       !Number.isInteger(input) &&
@@ -16,6 +24,7 @@ class RomanNumber {
       throw Error("invalid input");
     }
 
+    // check input is in correct range
     if (Number.isInteger(input) && (input < 1 || input > 3999)) {
       throw Error("invalid range");
     }
