e2d1fb18a5906e36326d864e0f4088f1601c04d7
Kuba Karmiński <jlkarminski@gmail.com>
2020-11-13 16:33:48 +0100

    Create a value map

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 class RomanNumber {
+  #VALUE_MAP = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
   constructor(input) {
     this.validate(input);
     if (typeof input === "string") {
@@ -30,7 +46,7 @@ class RomanNumber {
     }
   }
 
-  #validateRomanNumber() {}
+  #validateRomanNumber(num) {}
 
   #convertToDecimal() {}
 
